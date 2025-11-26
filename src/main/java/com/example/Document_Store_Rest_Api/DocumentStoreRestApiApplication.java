7bf97ab713af91b8c394aaa08e9dbc900ec8130f
Manoj18121812 <gavhaneam@gmail.com>
2025-11-26 17:38:53 +0530

    Document Store rest Api creation start

---
+++ src/main/java/com/example/Document_Store_Rest_Api/DocumentStoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Document_Store_Rest_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentStoreRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentStoreRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Document_Store_Rest_Api/DocumentStoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Document_Store_Rest_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentStoreRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
